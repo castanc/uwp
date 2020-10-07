556b6a1f9569326d4d2be50a505c153b737fcbce
castanc <54039892+castanc@users.noreply.github.com>
2020-10-07 13:04:24 -0300

    Create deviceinfo2.cs

---
+++ deviceinfo2.cs
@@ -0,0 +1,3 @@
+                //var hostNames2 = Windows.Networking.Connectivity.NetworkInformation.GetHostNames();
+                //var devieinfo = hostNames2.FirstOrDefault(name => name.Type == HostNameType.DomainName)?.DisplayName ?? "Access Error";// System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                //string username = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
