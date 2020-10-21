368e4a8c64893cf8f5a2a220ad57a7d6440984c7
castanc <54039892+castanc@users.noreply.github.com>
2020-10-21 18:06:26 +0000

    Create DeskHelpParameters.cs

---
+++ DeskHelpParameters.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deskhelp_UWP
+{
+    public class DeskHelpParameters
+    {
+        public string CheckUserUrl { set; get; }
+        public string UpdateUserUrl { set; get; }
+        public string PublicKey { set; get; }
+        public string JsonTemplate { set; get; }
+    }
+}
